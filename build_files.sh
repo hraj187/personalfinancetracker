f88161aa32aff9aaa559d200bf43acf012e466cb
Hemraj Hemade <hemrajhemade7@gmail.com>
2025-09-20 11:02:35 +0530

    intial commit

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Collect static files
+python3 manage.py collectstatic --noinput
+
+# Create Vercel-compatible output directory
+mkdir -p .vercel/output/static
+cp -r staticfiles/ .vercel/output/static/
+
+python3 manage.py makemigrations
+python3 manage.py migrate
